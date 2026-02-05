b28c15e7dea791810483077b849d1a0fc9aae8df
hacker-is-undefeatable <williamstudymaterials@gmail.com>
2026-02-06 04:15:25 +0800

    fix: deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  // Optional: force port if 5173 conflicts
-  server: {
-    port: 5173,
-  },
-  // If entry file issues persist, force it (rarely needed)
-  // build: {
-  //   rollupOptions: {
-  //     input: '/src/main.jsx',
-  //   }
-  // }
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    // Optional: force port if 5173 conflicts
+    server: {
+      port: 5173,
+    },
+    define: {
+      'process.env': env
+    }
+  }
 })
